38aa127798f5823f94d1a021b2fa8b53c0ada935
danigb <danigb@gmail.com>
2023-03-19 11:17:35 +0100

    feat: ConnectMidiClient

--- site/src/ConnectMidi.tsx
+++ site/src/ConnectMidi.tsx
@@ -1,94 +1,18 @@
-import { useEffect, useRef, useState } from "react";
-import { Listener, WebMidi } from "webmidi";
+import dynamic from "next/dynamic";
+import { MidiInstrument } from "./ConnectMidiClient";
 
-function supportsMidi() {
-  return navigator.requestMIDIAccess !== undefined;
-}
+const Midi = dynamic(
+  () => import("./ConnectMidiClient").then((module) => module.ConnectMidi),
+  {
+    ssr: false,
+  }
+);
 
-type Instrument = {
-  start(note: { note: number; velocity: number }): void;
-  stop(note: { stopId: number }): void;
-};
-
-export function ConnectMidi({
-  instrument,
-}: {
-  instrument: Instrument | undefined;
+export function ConnectMidi(props: {
+  shouldRenderComponent: boolean;
+  instrument: MidiInstrument | undefined;
 }) {
-  const inst = useRef<Instrument | null>(null);
-  const [midiDeviceNames, setMidiDeviceNames] = useState<string[]>([]);
-  const [midiDeviceName, setMidiDeviceName] = useState("");
-  const [disconnectMidiDevice, setDisconnectMidiDevice] = useState<
-    Listener[] | undefined
-  >();
-  const [lastNote, setLastNote] = useState("");
+  const { shouldRenderComponent } = props;
 
-  useEffect(() => {
-    if (!supportsMidi()) return;
-    WebMidi.enable().then(() => {
-      const deviceNames = WebMidi.inputs.map((device) => device.name);
-      setMidiDeviceNames(deviceNames);
-      setMidiDeviceName(deviceNames[0]);
-    });
-  }, []);
-
-  inst.current = instrument ?? null;
-
-  if (!supportsMidi()) return null;
-
-  return (
-    <>
-      <button
-        className={
-          "px-1 rounded " +
-          (disconnectMidiDevice ? "bg-emerald-600" : "bg-zinc-700")
-        }
-        onClick={() => {
-          if (disconnectMidiDevice) {
-            setDisconnectMidiDevice(undefined);
-            disconnectMidiDevice.forEach((listener) => listener.remove());
-            return;
-          }
-          const device = WebMidi.inputs.find(
-            (device) => device.name === midiDeviceName
-          );
-          if (!device) return;
-          const listener = device.addListener("noteon", (event) => {
-            const noteOn = {
-              note: event.note.number,
-              velocity: (event as any).rawVelocity,
-            };
-            inst.current?.start(noteOn);
-            setLastNote(`${noteOn.note} (${noteOn.velocity})`);
-          });
-          const listenerOff = device.addListener("noteoff", (event) => {
-            inst.current?.stop({ stopId: event.note.number });
-            setLastNote("");
-          });
-
-          setDisconnectMidiDevice([
-            ...(Array.isArray(listener) ? listener : [listener]),
-            ...(Array.isArray(listenerOff) ? listenerOff : [listenerOff]),
-          ]);
-        }}
-      >
-        MIDI
-      </button>
-      <select
-        className="bg-zinc-700 rounded py-[2px]"
-        value={midiDeviceName}
-        onChange={(e) => {
-          const name = e.target.value;
-          setMidiDeviceName(name);
-        }}
-      >
-        {midiDeviceNames.map((name) => (
-          <option key={name} value={name}>
-            {name}
-          </option>
-        ))}
-      </select>
-      <div className="opacity-50">{lastNote}</div>
-    </>
-  );
+  return shouldRenderComponent ? <Midi {...props} /> : null;
 }
