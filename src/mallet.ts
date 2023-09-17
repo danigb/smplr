f8de73e2889369710cad700993c8c8a0182b8b99
danigb <danigb@gmail.com>
2023-09-17 20:13:01 +0200

    chore: Mallet uses Varsilian

--- src/mallet.ts
+++ src/mallet.ts
@@ -1,48 +1,46 @@
-import { SfzInstrument } from "./sfz/sfz-kits";
-import { SfzSampler, SfzSamplerConfig } from "./sfz/sfz-sampler";
+import { Versilian, VersilianOptions } from "./versilian";
 
 export function getMalletNames() {
   return Object.keys(NAME_TO_PATH) as MalletName[];
 }
 
-export class Mallet extends SfzSampler {
-  constructor(
-    context: AudioContext,
-    options: Partial<SfzSamplerConfig> & { instrument: string }
-  ) {
-    const instrument = getMallet(options.instrument);
+export class Mallet extends Versilian {
+  constructor(context: AudioContext, options: VersilianOptions) {
     super(context, {
       ...options,
-      instrument,
+      instrument: NAME_TO_PATH[options.instrument ?? ""],
     });
   }
 }
 
-function getMallet(name: MalletName | string): SfzInstrument {
-  if (!NAME_TO_PATH[name]) throw Error(`Mallet instrument "${name}" not valid`);
-  return {
-    name: name,
-    websfzUrl: BASE_URL + NAME_TO_PATH[name] + EXT,
-  };
-}
 const BASE_URL = "https://danigb.github.io/samples/vcsl/";
 const EXT = ".websfz.json";
 
 type MalletName = keyof typeof NAME_TO_PATH;
 
 export const NAME_TO_PATH: Record<string, string | undefined> = {
-  "Balafon - Hard Mallet": "Struck Idiophones/balafon-hard-mallet",
-  "Balafon - Keyswitch": "Struck Idiophones/balafon-keyswitch",
-  "Balafon - Soft Mallet": "Struck Idiophones/balafon-soft-mallet",
+  "Balafon - Hard Mallet": "Idiophones/Struck Idiophones/Balafon - Hard Mallet",
+  "Balafon - Keyswitch": "Idiophones/Struck Idiophones/Balafon - Keyswitch",
+  "Balafon - Soft Mallet": "Idiophones/Struck Idiophones/Balafon - Soft Mallet",
   "Balafon - Traditional Mallet":
-    "Struck Idiophones/balafon-traditional-mallet",
-  "Tubular Bells 1": "Struck Idiophones/tubular-bells-1",
-  "Tubular Bells 2": "Struck Idiophones/tubular-bells-2",
-  "Vibraphone - Hard Mallets": "Struck Idiophones/vibraphone-hard-mallets",
-  "Vibraphone - Keyswitch": "Struck Idiophones/vibraphone-keyswitch",
-  "Vibraphone - Soft Mallets": "Struck Idiophones/vibraphone-soft-mallets",
-  "Xylophone - Hard Mallets": "Struck Idiophones/xylophone-hard-mallets",
-  "Xylophone - Keyswitch": "Struck Idiophones/xylophone-keyswitch",
-  "Xylophone - Medium Mallets": "Struck Idiophones/xylophone-medium-mallets",
-  "Xylophone - Soft Mallets": "Struck Idiophones/xylophone-soft-mallets",
+    "Idiophones/Struck Idiophones/Balafon - Traditional Mallet",
+
+  "Tubular Bells 1": "Idiophones/Struck Idiophones/Tubular Bells 1",
+  "Tubular Bells 2": "Idiophones/Struck Idiophones/Tubular Bells 2",
+
+  "Vibraphone - Bowed": "Idiophones/Struck Idiophones/Vibraphone - Bowed",
+  "Vibraphone - Hard Mallets":
+    "Idiophones/Struck Idiophones/Vibraphone - Hard Mallets",
+  "Vibraphone - Keyswitch":
+    "Idiophones/Struck Idiophones/Vibraphone - Keyswitch",
+  "Vibraphone - Soft Mallets":
+    "Idiophones/Struck Idiophones/Vibraphone - Soft Mallets",
+
+  "Xylophone - Hard Mallets":
+    "Idiophones/Struck Idiophones/Xylophone - Hard Mallets",
+  "Xylophone - Keyswitch": "Idiophones/Struck Idiophones/Xylophone - Keyswitch",
+  "Xylophone - Medium Mallets":
+    "Idiophones/Struck Idiophones/Xylophone - Medium Mallets",
+  "Xylophone - Soft Mallets":
+    "Idiophones/Struck Idiophones/Xylophone - Soft Mallets",
 } as const;
