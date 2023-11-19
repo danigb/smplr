a2e3c30b0cd16c073fec8948b83474744a8987f0
danigb <danigb@gmail.com>
2023-11-19 16:27:41 +0100

    fix: test

--- examples/standardized-audio-context-test/index.ts
+++ examples/standardized-audio-context-test/index.ts
@@ -1,11 +1,22 @@
 import { SplendidGrandPiano } from "smplr";
-import { AudioContext } from "standardized-audio-context";
+import { AudioContext as StandardizedAudioContext } from "standardized-audio-context";
 
 function main() {
-  const context = new AudioContext();
-  const piano = new SplendidGrandPiano(context);
+  const context = new StandardizedAudioContext();
+  const piano = new SplendidGrandPiano(context as unknown as AudioContext);
 
-  console.log("Hello world!");
+  const $button = document.getElementById("btn-test") as HTMLButtonElement;
+
+  piano.load.then(() => {
+    $button.disabled = false;
+  });
+
+  $button.addEventListener("click", () => {
+    const now = context.currentTime;
+    [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72].forEach((note, i) => {
+      piano.start({ note, time: now + i * 0.3 });
+    });
+  });
 }
 
 main();
