4ff14c0001775efc9221d5acd99e491417d9964e
danigb <danigb@gmail.com>
2026-01-18 19:07:27 +0100

    Add a comment on the iOS 17 bug

--- site/src/audio-context.ts
+++ site/src/audio-context.ts
@@ -1,5 +1,8 @@
 let context: AudioContext | undefined;
 
+// Note: iOS 17 and earlier have audio distortion due to sample rate mismatch bug.
+// This is a WebKit bug fixed in iOS 18. No JS workaround exists.
+// See: https://bugs.webkit.org/show_bug.cgi?id=154538
 export function getAudioContext() {
   context ??= new AudioContext();
   // Resume if suspended (required for iOS)
