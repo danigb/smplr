325a425ce7157dbfe66e4039b4c3a2f37b8d6523
danigb <danigb@gmail.com>
2026-01-18 18:51:17 +0100

    Try to fix demo site for iOS

--- site/src/audio-context.ts
+++ site/src/audio-context.ts
@@ -2,5 +2,9 @@ let context: AudioContext | undefined;
 
 export function getAudioContext() {
   context ??= new AudioContext();
+  // Resume if suspended (required for iOS)
+  if (context.state === "suspended") {
+    context.resume();
+  }
   return context;
 }
